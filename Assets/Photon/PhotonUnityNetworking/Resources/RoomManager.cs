d885e844f324f0b8bd3e58fd1b8e591ad3d42409
Devin <djfarmer@mtu.edu>
2021-12-27 21:08:24 -0500

    added photon and mac build

---
+++ Assets/Photon/PhotonUnityNetworking/Resources/RoomManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public string roomName = "Yeet";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom(roomName, null, null);
+    }
+
+}
